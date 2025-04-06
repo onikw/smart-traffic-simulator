4c6edbcae3c33d6f6fdc98df76febff13e152e5c
Wiktor Onik <wiktor.onik@gmail.com>
2025-04-06 19:52:11 +0200

    init

---
+++ src/main/java/com/github/onikw/smarttrafficsimulator/SmartTrafficSimulatorApplication.java
@@ -0,0 +1,15 @@
+package com.github.onikw.smarttrafficsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTrafficSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartTrafficSimulatorApplication.class, args);
+
+    }
+
+
+}
